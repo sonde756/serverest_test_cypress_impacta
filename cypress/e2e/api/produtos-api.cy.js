a92536c1e69e75b095c951a2b0c524b3550cd5b8
Santos <edson.santos@capgemini.com>
2024-05-23 15:58:18 -0300

    aula automação cypress 23/05

---
+++ cypress/e2e/api/produtos-api.cy.js
@@ -0,0 +1,89 @@
+/// <reference types="cypress" />
+import dados from '../../fixtures/usuario-api.json'
+const { faker, Faker } = require("@faker-js/faker");
+const urlBase = 'http://localhost:3000/'
+
+
+
+describe('API - Produtos', () => {
+
+    beforeEach(() => {
+        cy.token(dados.email, dados.senha).as('token')
+    });
+
+    it('Deve listar produtos com sucesso', () => {
+        cy.request({
+            method:'GET',
+            url:urlBase+'produtos'
+        }).then((response) =>{
+            expect(response.status).to.equal(200)
+            expect(response.duration).lessThan(1900)
+            expect(response.body).to.have.property('produtos')
+        })
+    });
+
+    it('Deve cadastra um produto com sucesso', () => {
+        cy.request({
+            method:'POST',
+            url:urlBase+'produtos',
+            body: {
+                "nome": faker.commerce.productName(),
+                "preco": 170,
+                "descricao": "124412",
+                "quantidade": 222
+              },
+              headers:{
+                authorization: this.token
+              }
+        }).then((response) =>{
+            expect(response.status).to.equal(201)
+            expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+        })
+    });
+
+    
+    it('Deve alterar dados do produto com sucesso', function () {
+        var produtoAlterado = `Produto teste ${Date.now()}`
+        cy.cadastrarProduto(this.token)
+            .then((response) => {
+                cy.log(response.body._id)
+                var id = response.body._id
+                cy.request({
+                    method: 'PUT',
+                    url: urlBase + 'produtos/' + id,
+                    body:
+                    {
+                        "nome": produtoAlterado,
+                        "preco": 100,
+                        "descricao": "Descrição alterada",
+                        "quantidade": 100
+                    },
+                    headers: {
+                        authorization: this.token
+                    }
+                }).then((response) => {
+                    expect(response.status).to.equal(200)
+                    expect(response.body.message).to.equal('Registro alterado com sucesso')
+                })
+            })
+    });
+
+    it('Deve excluir o produto com sucesso', function () {
+        cy.cadastrarProduto(this.token)
+            .then((response) => {
+                cy.log(response.body._id)
+                var id = response.body._id
+                cy.request({
+                    method: 'DELETE',
+                    url: urlBase + 'produtos/' + id,
+                    headers: {
+                        authorization: this.token
+                    }
+                }).then((response) => {
+                    expect(response.status).to.equal(200)
+                    expect(response.body.message).to.equal('Registro excluído com sucesso')
+                })
+            })
+    });
+
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -41,3 +41,105 @@ Cypress.Commands.add('login', (email, password) => {
         cy.get('[data-testid="checkbox"]').check()
         cy.get('[data-testid="cadastrar"]').click()
  })
+
+ Cypress.Commands.add('token', (email,senha) => {
+
+    cy.request({
+        method: 'POST',
+        url: 'https://serverest.dev/login',
+        body: 
+          {
+            "email": "fulano@qa.com",
+            "password": "teste"
+          }
+        })
+
+        Cypress.Commands.add('CadastroUsuarioAdmin', (nome, email, senha) => {
+            cy.visit('cadastrarusuarios')
+            cy.get('[data-testid="nome"]', ).clear().type(nome)
+            cy.get('[data-testid="email"]').clear().type(email)
+            cy.get('[data-testid="password"]').clear().type(senha)
+            cy.get('[data-testid="checkbox"]').check()
+            cy.get('[data-testid="cadastrar"]').click()
+         })
+        
+         Cypress.Commands.add('CadastroUsuarioComum', (nome, email, senha) => {
+            cy.visit('cadastrarusuarios')
+            cy.get('[data-testid="nome"]', ).clear().type(nome)
+            cy.get('[data-testid="email"]').clear().type(email)
+            cy.get('[data-testid="password"]').clear().type(senha)
+            cy.get('[data-testid="cadastrar"]').click()
+         })
+
+
+
+
+ })
+
+
+ Cypress.Commands.add('token', (email, senha) => {
+      cy.request({
+         method: 'POST', 
+         url: 'http://localhost:3000/login',
+         body: 
+            {
+               "email": email,
+               "password": senha
+            }
+      }).then((response) =>{
+         expect(response.status).equal(200)
+         return response.body.authorization
+      })
+ })
+
+
+ Cypress.Commands.add('cadastrarProduto', (tkn) =>{
+   var produto = `Produto teste ${Date.now()}`
+   cy.request({
+       method: 'POST',
+       url:  'http://localhost:3000/produtos',
+       body: {
+           "nome": produto,
+           "preco": 1001,
+           "descricao": "Comandos customizados...",
+           "quantidade": 1001
+         },
+         headers: {
+           authorization: tkn
+         }
+   })
+ })
+
+
+ Cypress.Commands.add('token', (email, senha) => {
+    cy.request({
+       method: 'POST', 
+       url: 'http://localhost:3000/login',
+       body: 
+          {
+             "email": email,
+             "password": senha
+          }
+    }).then((response) =>{
+       expect(response.status).equal(200)
+       return response.body.authorization
+    })
+})
+
+
+Cypress.Commands.add('cadastrarProduto', (tkn) =>{
+ var produto = `Produto teste ${Date.now()}`
+ cy.request({
+     method: 'POST',
+     url:  'http://localhost:3000/produtos',
+     body: {
+         "nome": produto,
+         "preco": 1001,
+         "descricao": "Comandos customizados...",
+         "quantidade": 1001
+       },
+       headers: {
+         authorization: tkn
+       }
+ })
+})
