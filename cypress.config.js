3712e479d2a9674325445842b7f4d9e93911d246
Santos <edson.santos@capgemini.com>
2024-05-28 10:58:39 -0300

    allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
+
 
 module.exports = defineConfig({
   video: true, // Enables video recording for all tests by default
 
   e2e: {
     setupNodeEvents(on, config) {
-      // Implement any custom node event listeners here (optional)
+      allureCypress(on);
     },
     baseUrl: 'https://front.serverest.dev/', // Base URL for your tests
     video: true, // Sets video recording specifically for e2e tests (redundant with top-level video: true)
